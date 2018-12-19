a4f9b5c9dda8d0d163a121bc9654ea046a9ceffd
GhazaryanShogher <shogherghazaryan4@gmail.com>
2018-12-20 01:06:57 +0400

    done

---
+++ js/homework_19_12.js
@@ -0,0 +1,19 @@
+var str = 'abacddbec';
+var arr1 = str.split('');
+var cnt = 0;
+console.log(arr1);
+
+function checkUnique(arr){
+    for(var i = 0; i<arr.length; i++){
+        for(var j=0; j<arr.length;j++){
+            if(arr[i]==arr[j]){
+                cnt++;
+            } 
+            console.log("cnt",cnt);
+        }
+        if(cnt == 1){
+            return arr[i];
+        } else cnt = 0;             
+    }
+}
+console.log(checkUnique(arr1));
