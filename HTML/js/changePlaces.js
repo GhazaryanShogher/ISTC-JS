cc351a91b3cac1ced2396c3ea51c3cbd723b9bb1
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-01-15 13:59:39 +0400

    change places

---
+++ HTML/js/changePlaces.js
@@ -0,0 +1,11 @@
+function red(){
+    document.getElementById("red-circle").style.animationName = "red";
+}
+function black(){
+    document.getElementById("black-circle").style.animationName = "black";
+}
+
+function change(){
+    red();
+    black();
+}
