bc401fd8faa49912171f3705a72228a30b1102aa
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-01-15 21:22:00 +0400

    changes

--- HTML/js/changePlaces.js
+++ HTML/js/changePlaces.js
@@ -4,8 +4,24 @@ function red(){
 function black(){
     document.getElementById("black-circle").style.animationName = "black";
 }
+function red1(){
+    document.getElementById("red-circle").style.animationName = "red1";
+}
+function black1(){
+    document.getElementById("black-circle").style.animationName = "black1";
+}
 
 function change(){
-    red();
-    black();
+    var cnt  = true;
+    if(cnt) {
+        red();
+        black();
+        cnt = false;
+    } else { 
+        black1();
+        red1();
+        cnt = true;
+    }    
+
+    
 }
--- HTML/js/famous.js
+++ HTML/js/famous.js
@@ -1,6 +1,5 @@
 function menu(){
     var element = document.getElementById("menu");
-    console.log(element.length);
    if(element.classList.contains("hidden")){
        element.classList.remove("hidden");       
    } else{
