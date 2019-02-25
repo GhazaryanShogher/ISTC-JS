2c23c71d1c4246c828b81e3eb6f1c145c0508aaa
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-02-25 21:44:28 +0400

    palidrome

--- js/array.js
+++ js/array.js
@@ -9,3 +9,16 @@ var points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){return a - b}); // 1,5,10,25,40,100
 var points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){return b - a}); // 100,40,25,10,5,1
+
+
+//palidrom
+function check(str){
+    var string = str.toLowerCase().replace(/[\W_]/g,'');
+    console.log('string = ',string);
+    var strRevers = string.split('').reverse().join('');
+    console.log('strRevers = ',strRevers);
+    if(string==strRevers){
+      return true;
+    } else return false;
+  }
+  document.write(check('car jkij raC'));
